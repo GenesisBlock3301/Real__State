f957b6ed8a920cece5004ecfb1b094a2b9a61805
NurAminSifatTanha <nur15-1463@diu.edu.bd>
2021-02-27 19:06:24 +0600

    first commite

---
+++ realState/accounts/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
+
+
+class SignupView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self,request,format=None):
+        data = self.request.data
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        passwor2 = data['password2']
+        if password == passwor2:
+            if User.objects.filter(email=email).exists():
+                return Response({'error':'Email already exists'})
+            else:
+                if len(password) < 6:
+                    return Response({'error':"Password must be at least 6 character"})
+                else:
+                    user = User.objects.create_user(email=email,password=password,name=name)
+                    user.save()
+                    return Response({"success":"User created successfully"})
+        else:
+            return Response({'error':"Password not match"})
